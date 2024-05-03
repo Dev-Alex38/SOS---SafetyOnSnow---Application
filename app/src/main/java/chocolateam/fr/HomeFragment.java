a31c4b188448c9f28e8f90b6b681cf7e09f62109
Alex <alexislavesvre@gmail.com>
2024-05-03 11:03:36 +0200

    bottom nav view

--- app/src/main/java/chocolateam/fr/HomeFragment.java
+++ app/src/main/java/chocolateam/fr/HomeFragment.java
@@ -1,18 +1,44 @@
 package chocolateam.fr;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button buttonProblem = rootView.findViewById(R.id.bouton_problem);
+
+        buttonProblem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProblemPopup();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void ProblemPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View popupView = inflater.inflate(R.layout.popup_valid_problem, null);
+        builder.setView(popupView);
+        final AlertDialog problem_popup = builder.create();
+        Button buttonClose = popupView.findViewById(R.id.button_close);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {problem_popup.dismiss();}});
+        problem_popup.setCancelable(false);
+        problem_popup.show();
     }
 }
